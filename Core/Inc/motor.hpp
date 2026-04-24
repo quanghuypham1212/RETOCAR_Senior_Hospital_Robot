c5c47b4c1bc6c3c4bb00c3329ddd81bdbcdd3df6
quanghuy1212 <quanghuy1212200404@gmail.com>
2026-04-24 23:52:51 +0700

    Clean up ignored files and add STM32 source

---
+++ Core/Inc/motor.hpp
@@ -0,0 +1,18 @@
+/*
+ * motor.hpp
+ *
+ *  Created on: Apr 24, 2026
+ *      Author: huy
+ */
+
+#ifndef SRC_MOTOR_HPP_
+#define SRC_MOTOR_HPP_
+
+class motor {
+public:
+	motor();
+	virtual ~motor();
+	
+};
+
+#endif /* SRC_MOTOR_HPP_ */
